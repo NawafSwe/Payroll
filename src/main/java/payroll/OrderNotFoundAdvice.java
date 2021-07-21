1d4c3dcaef9fb77e2580be8db1d9ab0e42b7895b
NawafSwe <nawaf.jr9@hotmail.com>
2021-07-21 17:19:18 +0300

    fix: marking order advice with @ControllerAdvice

--- src/main/java/payroll/OrderNotFoundAdvice.java
+++ src/main/java/payroll/OrderNotFoundAdvice.java
@@ -1,6 +1,7 @@
 package payroll;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+@ControllerAdvice
 public class OrderNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(OrderNotFoundException.class)
